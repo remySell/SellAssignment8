e343979eb254099a67e8609fb8def331069e8339
remySell <r.sell@setonhill.edu>
2020-05-04 15:23:08 -0400

    Ready to review author name remySell

--- SellAssignment8/ViewController.swift
+++ SellAssignment8/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBAction func Tap(_ sender: Any) {
+
+        outputLabel.text = "Tapped it!"
+    }
+    
+    
+    @IBAction func Swipe(_ sender: Any) {
+        
+        
+        outputLabel.text = "Swiped it!"
+    }
+    
+    @IBAction func Pinch(_ sender: Any) {
+        
+        outputLabel.text = "Ouch!"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
